f1cae44d4af968fc9765ea8dc0911a2c00261768
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-08-04 20:50:36 +0200

    Adds bb_system with not_nil template.

---
+++ bb_system.nim
@@ -0,0 +1,30 @@
+## `Badger bits <https://github.com/gradha/badger_bits>`_ system helpers.
+##
+## Contains stuff which I would like to see in `system
+## <http://nimrod-lang.org/system.html>`_ or is common to my code for some
+## reason.
+
+template not_nil*[T](x: T): bool =
+  ## Negated version of `system.isNil()`.
+  ##
+  ## `system.isNil <http://nimrod-lang.org/system.html#isNil,T>`_ is awkward to
+  ## use with assertions:
+  ##
+  ## .. code-block:: nimrod
+  ##   assert x.not_nil
+  ##   # The following does not compute.
+  ##   #assert not x.is_nil
+  ##   # Parenthesis, parenthesis everywhere.
+  ##   assert(not x.is_nil)
+  (not x.isNil)
+
+proc test() =
+  ## Micro test for this module.
+  var s: string
+  assert s.is_nil
+  s = ""
+  assert(not s.is_nil)
+  assert s.not_nil
+  s = nil
+
+when isMainModule: test()
