f8359ab99211a837ca0a5bfdc5860078980e6c6b
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-08-04 23:26:07 +0200

    Adds bb_os module.

---
+++ bb_os.nim
@@ -0,0 +1,26 @@
+## `Badger bits <https://github.com/gradha/badger_bits>`_ os helpers.
+##
+## Contains stuff which I would like to see in `os
+## <http://nimrod-lang.org/os.html>`_ or is common to my code for some reason.
+
+import os, strutils
+export os
+
+iterator dot_walk_dir_rec*(dir: string,
+    filter = {pcFile, pcDir}): string {.tags: [FReadDir].} =
+  ## Version of os.walkDirRec which ignores items starting with a dot.
+  ##
+  ## Since a `fix <http://forum.nimrod-lang.org/t/514>`_ for
+  ## `os.walkDirRec <http://nimrod-lang.org/os.html#walkDirRec>`_ won't come,
+  ## this version simply ignores all directories and files starting with a dot.
+  var stack = @[dir]
+  while stack.len > 0:
+    for k,p in walkDir(stack.pop()):
+      if k in filter:
+        case k
+        of pcFile, pcLinkToFile:
+          if p.find("/.") < 0:
+            yield p
+        of pcDir, pcLinkToDir:
+          if p.find("/.") < 0:
+            stack.add(p)
--- tests/tests.nim
+++ tests/tests.nim
@@ -1,7 +1,7 @@
-import bb_system
+import bb_system, bb_os, sequtils, algorithm
 
-proc test() =
-  ## Micro test for this module.
+
+proc test_not_nil() =
   var s: string
   assert s.is_nil
   s = ""
@@ -9,6 +9,39 @@ proc test() =
   assert s.not_nil
   s = nil
 
+
+proc test_dot_walk_dir_rec() =
+  const
+    dir = "dot_walk_dir_rec"
+    good_result = [
+      "dot_walk_dir_rec/this_also",
+      "dot_walk_dir_rec/this_yes",
+      "dot_walk_dir_rec/valid_dir/a",
+      "dot_walk_dir_rec/valid_dir/b"]
+    bad_result = [
+      "dot_walk_dir_rec/.ignore_dir/invisible",
+      "dot_walk_dir_rec/.ignore_dir/maybe_not",
+      "dot_walk_dir_rec/.ignore_this_file",
+      "dot_walk_dir_rec/this_also",
+      "dot_walk_dir_rec/this_yes",
+      "dot_walk_dir_rec/valid_dir/.ignore_this_file",
+      "dot_walk_dir_rec/valid_dir/a",
+      "dot_walk_dir_rec/valid_dir/b"]
+
+  var l = to_seq(dir.dot_walk_dir_rec)
+  l.sort(system.cmp)
+  assert l == @good_result
+  assert l != @bad_result
+  l = to_seq(dir.walk_dir_rec)
+  l.sort(system.cmp)
+  assert l == @bad_result
+  assert l != @good_result
+
+
+proc test() =
+  test_not_nil()
+  test_dot_walk_dir_rec()
   echo "All tests run"
+
 
 when isMainModule: test()
