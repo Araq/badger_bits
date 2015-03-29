dc56bfe971fa72efecb1718e1abc032e9ca52904
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2015-03-29 16:06:25 +0200

    Implements dot_walk_dir. Refs #16.

--- bb_os.nim
+++ bb_os.nim
@@ -6,6 +6,13 @@
 import os, strutils
 export os
 
+
+when defined(windows):
+  import winlean
+elif defined(posix):
+  import posix
+
+
 iterator dot_walk_dir_rec*(dir: string,
     filter = {pcFile, pcDir}): string {.tags: [ReadDirEffect].} =
   ## Version of os.walkDirRec which ignores items starting with a dot.
@@ -24,3 +31,40 @@ iterator dot_walk_dir_rec*(dir: string,
         of pcDir, pcLinkToDir:
           if p.find("/.") < 0:
             stack.add(p)
+
+
+iterator dot_walk_dir*(dir: string):
+    tuple[kind: PathComponent, path: string] {.tags: [ReadDirEffect].} =
+  ## Version of os.walkDir which ignores items starting with a dot.
+  ##
+  ## The windows version will report files with a dot.
+  when defined(windows):
+    var f: TWIN32_FIND_DATA
+    var h = findFirstFile(dir / "*", f)
+    if h != -1:
+      while true:
+        var k = pcFile
+        if not skipFindData(f):
+          if (f.dwFileAttributes and FILE_ATTRIBUTE_DIRECTORY) != 0'i32:
+            k = pcDir
+          if (f.dwFileAttributes and FILE_ATTRIBUTE_REPARSE_POINT) != 0'i32:
+            k = succ(k)
+          yield (k, dir / extractFilename(getFilename(f)))
+        if findNextFile(h, f) == 0'i32: break
+      findClose(h)
+  else:
+    var d = opendir(dir)
+    if d != nil:
+      while true:
+        var x = readdir(d)
+        if x == nil: break
+        var y = $x.d_name
+        if y[0] != '.' and y != "..":
+          var s: TStat
+          y = dir / y
+          if lstat(y, s) < 0'i32: break
+          var k = pcFile
+          if S_ISDIR(s.st_mode): k = pcDir
+          if S_ISLNK(s.st_mode): k = succ(k)
+          yield (k, y)
+      discard closedir(d)
